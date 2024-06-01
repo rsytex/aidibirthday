3fc5a051b93354fdee5a52a6e0663ba3b80d2cae
rsytex <117422480+rsytex@users.noreply.github.com>
2024-06-02 00:32:57 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# aidibirthday
+# HappyBirthDay
---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $(function () {
+    var flame = $("#flame");
+    var txt = $("h1");
+
+    flame.on({
+        click: function () {
+        flame.removeClass("burn").addClass("puff");
+        $(".smoke").each(function () {
+            $(this).addClass("puff-bubble");
+        });
+        $("#glow").remove();
+        txt.hide().html("I know every wish u made would be came true, have a great 18th of your life =)").delay(750).fadeIn(300);
+        $("#candle").animate(
+            {
+            opacity: ".5"
+            },
+            100
+        );
+        }
+    });
+});});
+
